4a08abc5e8c7cd2d6e6f3cc22eeb1c3b1ed659ef
Sabi <84113149+marxmaler@users.noreply.github.com>
2022-01-18 23:14:48 +0900

    alba scraper(알바천국) page calculation added

--- alba_scraper/functions.py
+++ alba_scraper/functions.py
@@ -1,7 +1,10 @@
+import math
+
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import csv
+
 
 # brand별 모집 공고 페이지 url 추출
 def extract_brand_url():
@@ -11,43 +14,51 @@ def extract_brand_url():
     urls = []
     companies = []
     for li in lis:
-        a = li.find("a")
-        span = li.find("span", class_="company")
-        urls.append(a["href"])
+        links = li.findAll("a", class_="brandHover")
+        if links != []:
+            for link in links:
+                urls.append(link["href"])
+                span = link.find("span", class_="company")
+                if "." in span.text:
+                    company = span.text.split(".")[0]
+                    company = company.rstrip(" >")
+                    companies.append(company)
+                elif "/" in span.text:
+                    company = span.text.replace("/", "&")
+                    company = company.rstrip(" >")
+                    companies.append(company)
+                else:
+                    company = span.text.rstrip(" >")
+                    companies.append(company)
 
-        if "." in span.text:
-            company = span.text.split(".")[0]
-            companies.append(company)
-        elif "/" in span.text:
-            company= span.text.replace("/", "&")
-            companies.append(company)
-        else:
-            companies.append(span.text)
 
     return urls, companies
+
 
 # 페이지 데이터 추출
 def extract_page_data(urls):
     jobs_list = []
 
     for url in tqdm(urls):
-        page = 1
         jobs = []
-        while True:
-            paged_url = f"{url}job/brand/?page={page}&pagesize = 50"
-            page +=1
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, "html.parser")
+        jobCount = soup.find("p", class_="jobCount")
+        if(jobCount == None):
+            continue
+        lastPage = math.ceil(int(jobCount.find("strong").text.replace(",", ""))/50)
+        for i in range(1, lastPage+1):
+            paged_url = f"{url}job/brand/?page={i}&pagesize = 50"
             res = requests.get(paged_url)
             soup = BeautifulSoup(res.text, "html.parser")
             tbody = soup.find("tbody")
-            try:
-              trs = tbody.find_all("tr")
-            except:
-              break
-            if len(trs)<2:
+            trs = tbody.find_all("tr")
+
+            if len(trs) < 2:
                 break
             for tr in trs:
                 tds = tr.find_all("td")
-                if len(tds)<2: #공고 요약 보기 들어있는 tr 거르기
+                if len(tds) < 2:  # 공고 요약 보기 들어있는 tr 거르기
                     continue
                 try:
                     place = tds[0].text
@@ -60,7 +71,7 @@ def extract_page_data(urls):
                         "title": title,
                         "time": time,
                         "pay": pay,
-                        "date":date,
+                        "date": date,
                     }
                     jobs.append(job)
                 except:
@@ -69,9 +80,11 @@ def extract_page_data(urls):
 
     return jobs_list
 
+
 # csv로 저장
-def save_as_csv(jobs, company_name):
-    file = open(f"{company_name}.csv", mode="w", encoding="utf-8")
-    writer = csv.writer(file)
-    for job in tqdm(jobs):
-        writer.writerow(list(job.values()))
+def save_as_csv(jobs_list, company_names):
+    for i in range(len(jobs_list)):
+        file = open(f"{company_names[i]}.csv", mode="w", encoding="utf-8")
+        writer = csv.writer(file)
+        for job in tqdm(jobs_list[i]):
+            writer.writerow(list(job.values()))
--- alba_scraper/main.py
+++ alba_scraper/main.py
@@ -1,7 +1,5 @@
 from functions import extract_brand_url, extract_page_data, save_as_csv
 
 urls, companies = extract_brand_url()
-print(companies)
-# jobs_list = extract_page_data(urls)
-# for i in range(len(jobs_list)):
-#     save_as_csv(jobs_list[i], companies[i])
+jobs_list = extract_page_data(urls)
+save_as_csv(jobs_list, companies)
